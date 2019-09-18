218f8f75a604b98fe464f3fbcefb51068639bb4f
alanoudssr <alanoudssr@gmail.com>
2019-09-18 20:43:56 +0300

    change thought sprite

--- src/scenes/GhostScene.js
+++ src/scenes/GhostScene.js
@@ -1,6 +1,7 @@
 import { CST } from "../CST";
 import Phaser from 'phaser';
 import dude from "../assets/dude.png";
+import sparkle from "../assets/sparkle.png";
 import leftMap from "../assets/maps/leftMap.json";
 import darkTileSet from "../assets/maps/tilesets/darkTileSet.png";
 import fontPng from "../assets/fonts/bitmap/chiller.png"
@@ -31,7 +32,7 @@ export default class GhostScene extends Phaser.Scene {
         this.load.tilemapTiledJSON("mapG", leftMap);
         this.load.bitmapFont('desyrel', fontPng, fontXml);
         this.load.spritesheet('ghost', dude, { frameWidth: 32, frameHeight: 48 });
-        this.load.spritesheet('mailMan', dude, { frameWidth: 32, frameHeight: 48 });
+        this.load.spritesheet('mailMan', sparkle, { frameWidth: 32, frameHeight: 48 });
     }
 
     // create functions5
@@ -55,46 +56,40 @@ export default class GhostScene extends Phaser.Scene {
         const spawnPoint = map.findObject("Objects", obj => obj.name === "Spawn Point");
         const waitingMail = map.findObject("Objects", obj => obj.name === "waiting for mail");
 
-        this.ghost = this.physics.add.sprite(spawnPoint.x, spawnPoint.y, 'ghost');
         this.mailMan = this.physics.add.sprite(waitingMail.x, waitingMail.y, 'mailMan');
+        this.ghost = this.physics.add.sprite(spawnPoint.x, spawnPoint.y, 'ghost');
 
         camera.startFollow(this.ghost);
         camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 
 
+        this.mailMan.visible = false
 
-        this.physics.add.overlap(this.ghost, this.mailMan, mailFun, null, this);
+        this.physics.add.overlap(this.ghost, this.mailMan, mailFun, null, this)
 
         function mailFun() {
-            if (!this.overlapTriggered) {
-
-                /// text
-                this.path = new Phaser.Curves.Path(waitingMail.x, waitingMail.y);
-
-                this.path.lineTo(waitingMail.x + 1000, waitingMail.y);
-                this.path.splineTo([waitingMail.x, waitingMail.y, waitingMail.x, waitingMail.y - 200, waitingMail.x, waitingMail.y - 300, waitingMail.x, waitingMail.y - 400, waitingMail.x, waitingMail.y - 500]);
-                this.path.lineTo(waitingMail.x, waitingMail.y);
-
-                var text = this.add.dynamicBitmapText(0, 0, 'desyrel', 'Waiting', 44);
-
-                text.setDisplayCallback(this.positionOnPath);
-
-                var graphics = this.add.graphics();
-
-                graphics.lineStyle(0, 0xffffff, 1);
-
-                this.path.draw(graphics, 128);
-                ///
+            this.mailMan.visible = true
 
 
+            /// text
+            this.path = new Phaser.Curves.Path(waitingMail.x + 1000, 2500);
 
-                this.overlapTriggered = true
-                console.log('hi');
+            this.path.lineTo(waitingMail.x + 1000, 2500);
+            this.path.splineTo([waitingMail.x, waitingMail.y, waitingMail.x, waitingMail.y - 200, waitingMail.x, waitingMail.y - 300, waitingMail.x, waitingMail.y - 400, waitingMail.x, waitingMail.y - 500]);
+            this.path.lineTo(waitingMail.x, 2500);
 
-                setTimeout(() => {
-                    this.overlapTriggered = false
-                }, 2000);
-            }
+            var text = this.add.dynamicBitmapText(0, 0, 'desyrel', 'Waiting', 44);
+
+            text.setDisplayCallback(this.positionOnPath);
+
+            var graphics = this.add.graphics();
+
+            graphics.lineStyle(0, 0xffffff, 1);
+
+            this.path.draw(graphics, 128);
+            ///
+
+
         }
 
         this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
@@ -122,7 +117,15 @@ export default class GhostScene extends Phaser.Scene {
             repeat: -1
         });
 
+        this.anims.create({
+            key: 'round',
+            frames: this.anims.generateFrameNumbers('mailMan', { start: 0, end: 4 }),
+            frameRate: 5,
+            repeat: -1
+        });
 
+
+        this.mailMan.anims.play('round', true)
 
 
 
@@ -130,6 +133,7 @@ export default class GhostScene extends Phaser.Scene {
 
     // update functions 
     update() {
+
 
         //
         this.t += 0.001;
@@ -163,7 +167,6 @@ export default class GhostScene extends Phaser.Scene {
             this.ghost.setVelocityX(0);
             this.ghost.anims.play('turn');
         }
-
     }
     positionOnPath(data) {
         var pathVector = this.path.getPoint(this.t + ((6 - data.index) * 0.04));
