1b0439f38da3dbc3122119e7f60987cee716b9ed
alanoudssr <alanoudssr@gmail.com>
2019-09-18 17:52:17 +0300

    add text

--- src/scenes/GhostScene.js
+++ src/scenes/GhostScene.js
@@ -3,6 +3,9 @@ import Phaser from 'phaser';
 import dude from "../assets/dude.png";
 import leftMap from "../assets/maps/leftMap.json";
 import darkTileSet from "../assets/maps/tilesets/darkTileSet.png";
+import fontPng from "../assets/fonts/bitmap/chiller.png"
+import fontXml from "../assets/fonts/bitmap/chiller.xml"
+
 export default class GhostScene extends Phaser.Scene {
     constructor(config) {
         super({
@@ -15,6 +18,9 @@ export default class GhostScene extends Phaser.Scene {
         this.keyA;
         this.keyS;
         this.keyD;
+        this.t = 0;
+        this.path;
+        this.positionOnPath = this.positionOnPath.bind(this)
     }
 
     init() {
@@ -23,12 +29,16 @@ export default class GhostScene extends Phaser.Scene {
     preload() {
         this.load.image("tilesG", darkTileSet);
         this.load.tilemapTiledJSON("mapG", leftMap);
+        this.load.bitmapFont('desyrel', fontPng, fontXml);
         this.load.spritesheet('ghost', dude, { frameWidth: 32, frameHeight: 48 });
         this.load.spritesheet('mailMan', dude, { frameWidth: 32, frameHeight: 48 });
     }
 
     // create functions5
     create() {
+
+
+
         let { width, height } = this.sys.game.canvas;
         this.cameras.main.setViewport(0, 0, width / 2, height);
 
@@ -51,11 +61,33 @@ export default class GhostScene extends Phaser.Scene {
         camera.startFollow(this.ghost);
         camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 
+
+
         this.physics.add.overlap(this.ghost, this.mailMan, mailFun, null, this);
 
         function mailFun() {
             if (!this.overlapTriggered) {
-                this.add.text(spawnPoint.x, spawnPoint.y, 'Hello World', { fontFamily: '"Roboto Condensed"' });
+
+                /// text
+                this.path = new Phaser.Curves.Path(waitingMail.x, waitingMail.y);
+
+                this.path.lineTo(waitingMail.x + 1000, waitingMail.y);
+                this.path.splineTo([waitingMail.x, waitingMail.y, waitingMail.x, waitingMail.y - 200, waitingMail.x, waitingMail.y - 300, waitingMail.x, waitingMail.y - 400, waitingMail.x, waitingMail.y - 500]);
+                this.path.lineTo(waitingMail.x, waitingMail.y);
+
+                var text = this.add.dynamicBitmapText(0, 0, 'desyrel', 'Waiting', 44);
+
+                text.setDisplayCallback(this.positionOnPath);
+
+                var graphics = this.add.graphics();
+
+                graphics.lineStyle(0, 0xffffff, 1);
+
+                this.path.draw(graphics, 128);
+                ///
+
+
+
                 this.overlapTriggered = true
                 console.log('hi');
 
@@ -98,6 +130,15 @@ export default class GhostScene extends Phaser.Scene {
 
     // update functions 
     update() {
+
+        //
+        this.t += 0.001;
+
+        if (this.t >= (1 - 0.24)) {
+            this.t = 0;
+        }
+        //
+
         let { width, height } = this.sys.game.canvas;
         this.cameras.main.setViewport(0, 0, width / 2, height);
 
@@ -123,5 +164,15 @@ export default class GhostScene extends Phaser.Scene {
             this.ghost.anims.play('turn');
         }
 
+    }
+    positionOnPath(data) {
+        var pathVector = this.path.getPoint(this.t + ((6 - data.index) * 0.04));
+
+        if (pathVector) {
+            data.x = pathVector.x;
+            data.y = pathVector.y;
+        }
+
+        return data;
     }
 }
