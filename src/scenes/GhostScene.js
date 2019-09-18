32c80f56604bd6909afcbcc6b9865e91cd6853f2
Hazim Alblowi <hazim.alblowi@gmail.com>
2019-09-18 13:43:07 +0300

    Refactor

--- src/scenes/GhostScene.js
+++ src/scenes/GhostScene.js
@@ -1,87 +1,84 @@
 import { CST } from "../CST";
 import Phaser from 'phaser';
 import dude from "../assets/dude.png";
-
+import leftMap from "../assets/maps/leftMap.json";
+import darkTileSet from "../assets/maps/tilesets/darkTileSet.png";
 export default class GhostScene extends Phaser.Scene {
-    constructor() {
+    constructor(config) {
         super({
             key: CST.SCENES.GHOST,
             active: true
-        })
+        });
+        this.speed = 160;
+        this.ghost;
+        this.keyW;
+        this.keyA;
+        this.keyS;
+        this.keyD;
     }
 
     init() {
 
     }
     preload() {
-        this.load.image("tilesG", "https://www.mikewesthad.com/phaser-3-tilemap-blog-posts/post-1/assets/images/escheresque_dark.png");
-        this.load.tilemapTiledJSON("mapG", "https://www.mikewesthad.com/phaser-3-tilemap-blog-posts/post-1/assets/tilemaps/tuxemon-town.json");
-
-        this.load.image("repeating-background", "https://www.mikewesthad.com/phaser-3-tilemap-blog-posts/post-1/assets/images/escheresque_dark.png");
-
+        this.load.image("tilesG", darkTileSet);
+        this.load.tilemapTiledJSON("mapG", leftMap);
         this.load.spritesheet('ghost', dude, { frameWidth: 32, frameHeight: 48 });
     }
 
     // create functions5
     create() {
         let { width, height } = this.sys.game.canvas;
-        this.cameras.main.setViewport(0, 0, width / 2, height / 2);
+        this.cameras.main.setViewport(0, 0, width / 2, height);
 
         const map = this.make.tilemap({ key: "mapG" });
 
-        const tileset = map.addTilesetImage("tuxmon-sample-32px-extruded", "tilesGs");
+        const tileset = map.addTilesetImage("darkTileSet", "tilesG");
 
         const belowLayer = map.createStaticLayer("Below Player", tileset, 0, 0);
-        const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
-        // const aboveLayer = map.createStaticLayer("Above Player", tileset, 0, 0);
 
 
         const camera = this.cameras.main;
 
-        console.log("mapG", map);
 
-
-        const bg = this.add.tileSprite(0, 0, map.widthInPixels, map.heightInPixels, "repeating-background");
-        bg.setOrigin(0, 0);
-        // worldLayer.setCollisionByProperty({ collides: true });
-        // aboveLayer.setDepth(10);
         const spawnPoint = map.findObject("Objects", obj => obj.name === "Spawn Point");
-        playerLeft = this.physics.add.sprite(300, 450, 'dude');
-        this.physics.add.collider(playerLeft, worldLayer);
-        camera.startFollow(playerLeft);
+        const waitingMail = map.findObject("Objects", obj => obj.name === "waiting for mail");
+
+        this.ghost = this.physics.add.sprite(spawnPoint.x, spawnPoint.y, 'ghost');
+        camera.startFollow(this.ghost);
         camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 
-        keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-        keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-        keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        this.physics.add.overlap(this.ghost, waitingMail, mailFun, null, this);
 
+        function mailFun() {
 
-        function interact() {
-            if (keyA.isDown) {
-                // emitter.emit('aPressed', obj)
-            }
         }
+
+        this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
         this.anims.create({
             key: 'left',
-            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frames: this.anims.generateFrameNumbers('ghost', { start: 0, end: 3 }),
             frameRate: 10,
             repeat: -1
         });
 
         this.anims.create({
             key: 'turn',
-            frames: [{ key: 'dude', frame: 4 }],
+            frames: [{ key: 'ghost', frame: 4 }],
             frameRate: 20
         });
 
         this.anims.create({
             key: 'right',
-            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frames: this.anims.generateFrameNumbers('ghost', { start: 5, end: 8 }),
             frameRate: 10,
             repeat: -1
         });
+
 
 
 
@@ -90,6 +87,30 @@ export default class GhostScene extends Phaser.Scene {
 
     // update functions 
     update() {
+        let { width, height } = this.sys.game.canvas;
+        this.cameras.main.setViewport(0, 0, width / 2, height);
+
+        this.ghost.setVelocity(0)
+
+
+        if (this.keyA.isDown) {
+            this.ghost.setVelocityX(-this.speed);
+            this.ghost.anims.play('left', true);
+        }
+        else if (this.keyD.isDown) {
+            this.ghost.setVelocityX(this.speed);
+            this.ghost.anims.play('right', true);
+        }
+        else if (this.keyW.isDown) {
+            this.ghost.setVelocityY(-this.speed);
+        }
+        else if (this.keyS.isDown) {
+            this.ghost.setVelocityY(this.speed);
+        }
+        else {
+            this.ghost.setVelocityX(0);
+            this.ghost.anims.play('turn');
+        }
 
     }
 }
