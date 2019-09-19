9430be47d0e09fbc7a5d58eb6cc8fa3a90180e12
reemharbi <reemharbi.3@gmail.com>
2019-09-19 17:11:04 +0300

    added npc

--- src/scenes/MemoryScene.js
+++ src/scenes/MemoryScene.js
@@ -2,6 +2,7 @@ import { CST } from "../CST";
 import Phaser from 'phaser';
 import dude from "../assets/dude.png";
 import cloud from "../assets/cloud.png";
+import smallCloud from "../assets/cloud3.png";
 import rightMap from "../assets/maps/rightMap.json";
 
 export default class MemoryScene extends Phaser.Scene {
@@ -24,6 +25,7 @@ export default class MemoryScene extends Phaser.Scene {
         this.load.tilemapTiledJSON("map", rightMap);
         this.load.spritesheet('dude', dude, { frameWidth: 32, frameHeight: 48 });
         this.load.image("cloud", cloud);
+        this.load.image("smallCloud", smallCloud);
     }
 
     // create functions
@@ -61,47 +63,28 @@ export default class MemoryScene extends Phaser.Scene {
         camera.setBounds(0, 0, map.widthInPixels+(width/ 2), map.heightInPixels);
 
         this.playerRight = this.physics.add.sprite(130, 450, 'dude');
-        this.cloud1 = this.physics.add.sprite(100, 100, 'cloud');
-        this.cloud2 = this.physics.add.sprite(500, 150, 'cloud');
-        this.cloud3 = this.physics.add.sprite(800, 100, 'cloud');
-        this.cloud4 = this.physics.add.sprite(1200, 150, 'cloud');
-        this.cloud5 = this.physics.add.sprite(100, 500, 'cloud');
-        this.cloud6 = this.physics.add.sprite(500, 550, 'cloud');
-        this.cloud7 = this.physics.add.sprite(800, 500, 'cloud');
-        this.cloud8 = this.physics.add.sprite(1200, 550, 'cloud');
-        this.cloud9 = this.physics.add.sprite(100, 940, 'cloud');
+        this.suicidalDude = this.physics.add.sprite(830, 40, 'dude');
+        this.overworkedDude = this.physics.add.sprite(200, 130, 'dude');
+        this.mayorDude = this.physics.add.sprite(1100, 290, 'dude');
+        this.prayingDude = this.physics.add.sprite(1170, 1225, 'dude');
+        this.lostLoveDude = this.physics.add.sprite(1020, 720, 'dude');
+        this.gardenerDude = this.physics.add.sprite(430, 500, 'dude');
+        this.bulliedDude = this.physics.add.sprite(900, 1100, 'dude');
+        this.mailDude = this.physics.add.sprite(100, 830, 'dude');
+        this.deadDude = this.physics.add.sprite(640, 830, 'dude');
+        // this.overworkedCloud = this.physics.add.sprite(100, 100, 'cloud');
+        this.cloud = this.physics.add.sprite(540, 10, 'smallCloud');
+        // this.suicidalCloud = this.physics.add.sprite(800, 100, 'cloud');
+        this.mayorCloud = this.physics.add.sprite(1200, 150, 'cloud');
+        this.mailCloud = this.physics.add.sprite(100, 600, 'cloud');
+        this.gardenerCloud = this.physics.add.sprite(500, 500, 'cloud');
+        this.cloud = this.physics.add.sprite(800, 500, 'cloud');
+        this.lostLoveCloud = this.physics.add.sprite(1050, 680, 'cloud');
+        this.holeCloud = this.physics.add.sprite(100, 940, 'cloud');
         this.cloud10 = this.physics.add.sprite(500, 990, 'cloud');
-        this.cloud11 = this.physics.add.sprite(800, 940, 'cloud');
-        this.cloud12 = this.physics.add.sprite(1200, 990, 'cloud');
+        this.bulliedCloud = this.physics.add.sprite(800, 940, 'cloud');
+        this.prayingCloud = this.physics.add.sprite(1100, 1200, 'cloud');
         
-
-        // this.anims.create({
-        //     key: 'left',
-        //     frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-        //     frameRate: 10,
-        //     repeat: -1
-        // });
-
-        // this.anims.create({
-        //     key: 'turn',
-        //     frames: [{ key: 'dude', frame: 4 }],
-        //     frameRate: 20
-        // });
-
-        // this.anims.create({
-        //     key: 'right',
-        //     frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-        //     frameRate: 10,
-        //     repeat: -1
-        // });
-
-        // this.anims.create({
-        //     key: 'round',
-        //     frames: this.anims.generateFrameNumbers('cloud', { start: 0, end: 4 }),
-        //     frameRate: 5,
-        //     repeat: -1
-        // });
-
 
     }
 
